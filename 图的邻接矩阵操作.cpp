11d033924c0bdeab8d4db2f26a53747685efef78
lzh <luzehua@zhongbei.com>
2019-08-28 10:49:01 +0800

    图的邻接矩阵操作1

--- 图的邻接矩阵操作.cpp
+++ 图的邻接矩阵操作.cpp
@@ -84,17 +84,13 @@ typedef struct  {
 	int vexnum, arcnum;  //图的当前顶点数与弧数
 	GraphKind kind;  //图的种类标志
 }MGraph;
-
-typedef struct closedg{
-	VertexType adjvex;
-	VRType lowcost;
-}closedge[MAX_VERTEX_NUM];
  
 bool visited[MAX];  //访问标志数组
 Status (*VisitFunc)(MGraph &G,int v);  //函数变量
- 
-int LocateVex(const MGraph &G, VertexType u)
-{
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>图的基本操作 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+
+int LocateVex(const MGraph &G, VertexType u){
 	//初始条件：图G存在，u与G中顶点有相同特征
 	//操作结果：若G中存在顶点u，则返回该顶点在图中位置，否则返回-1
 	for(int i=0; i < G.vexnum; ++i)
@@ -104,6 +100,153 @@ int LocateVex(const MGraph &G, VertexType u)
 	}
 	return -1;
 }
+
+void DisplayVexValue(const MGraph &G){
+	//显示所有的顶点
+	printf("顶点为：");
+	for(int i=0; i < G.vexnum; ++i)
+		printf("%c\t",G.vexs[i]);
+	printf("\n");
+}
+ 
+void DisplayAdjMatrix(const MGraph &G){
+	//输出邻接矩阵
+	printf("\n邻接矩阵为：\n");
+	for(int i=0; i<G.vexnum; ++i){
+		for(int j=0; j<G.vexnum; ++j){
+			if(G.arcs[i][j].adj==INFINITY){
+				printf("∞\t");
+			}
+			else{
+				printf("%d\t",G.arcs[i][j].adj);
+			}
+		}
+		printf("\n");
+	}
+}
+
+Status PutVex(MGraph &G,int v, VertexType value){
+	//初始条件：图G存在，v是G中某个顶点的序号
+	//操作结果：对v赋值value
+	if(v >= G.vexnum || v < 0)
+		return ERROR;
+	G.vexs[v] = value;
+	return OK;
+}
+
+Status InsertVex(MGraph &G, VertexType v){
+	//初始条件：图G存在，v和图G某个顶点有相同的特征
+	//操作结果：在图G中增添新顶点v（此时不增加关系）
+	int i;
+	G.vexs[G.vexnum] = v;  //赋于顶点值
+	for(i=0; i <=G.vexnum; ++i)
+	{
+		if(G.kind%2==0)//图
+		{
+			G.arcs[G.vexnum][i].adj = 0;
+			G.arcs[i][G.vexnum].adj = 0;
+		}
+		else  //网
+		{
+			G.arcs[G.vexnum][i].adj = INFINITY;
+			G.arcs[i][G.vexnum].adj = INFINITY;
+		}
+	}
+	G.arcs[G.vexnum][i].info = NULL; //初始化该弧的相关信息
+	G.arcs[i][G.vexnum].info = NULL; //初始化该弧的相关信息
+	G.vexnum++;
+	
+	return OK;
+}
+ 
+Status DeleteVex(MGraph &G, VertexType v){
+	//初始条件：图G存在，v是G中某个顶点
+	//操作结果：删除G中顶点v及其相关的弧
+	int k = LocateVex(G,v);
+	if(k == -1)
+		return ERROR; //无此结点
+	//1.从顶点向量中删除该顶点
+	int i,j;
+	for(i=k; i < G.vexnum-1; ++i)
+	{
+		G.vexs[i] = G.vexs[i+1];
+	}
+ 
+	//2.删除边的关系
+	for(i=k; i < G.vexnum-1; ++i)
+	{
+		for(j=0; j < G.vexnum; ++j)
+		{
+			G.arcs[i][j].adj = G.arcs[i+1][j].adj;
+			//因为没有设弧的相关信息，就不考虑G.arcs[][].info了
+		}
+	}
+	for(j=k; j < G.vexnum-1; ++j)
+	{
+		for(i=0; i < G.vexnum; ++i)
+		{
+			G.arcs[i][j].adj = G.arcs[i][j+1].adj;
+			//因为没有设弧的相关信息，就不考虑G.arcs[][].info了
+		}
+	}
+ 
+	--G.arcnum;  //边数减1
+	--G.vexnum;  //顶点数减1
+	return OK;
+}
+ 
+Status InsertArc(MGraph &G, VertexType v, VertexType w){
+	//初始条件：图G存在，v和w是G中两个顶点
+	//操作结果：在G中增添弧<v,w>，若是无向的，则还增添对称弧
+	int k1= LocateVex(G,v);
+	int k2= LocateVex(G,w);
+	if(k1==-1 || k2==-1)
+		return ERROR;  //图中不存在v或w
+	if(G.kind%2 == 1)//网
+	{
+		printf("请输入该弧的权值:");
+		scanf("%d",&G.arcs[k1][k2].adj);
+	}
+	else // 图
+	{
+		G.arcs[k1][k2].adj = 1;
+	}
+ 
+	if(G.kind > 1) //无向
+	{
+		G.arcs[k2][k1].adj = G.arcs[k1][k2].adj;
+	}
+	++G.arcnum;  //弧数加1
+	return OK;
+	
+}
+ 
+Status DeleteArc(MGraph &G, VertexType v, VertexType w){
+	//初始条件：图G存在，v和w是G中两个顶点
+	//操作结果：在G中删除弧<v,w>，若是无向的，则还删除对称弧
+	int k1= LocateVex(G,v);
+	int k2= LocateVex(G,w);
+	if(k1==-1 || k2==-1)
+		return ERROR;  //图中不存在v或w
+	if(G.kind%2 == 1)//网
+	{
+		G.arcs[k1][k2].adj = INFINITY;
+	}
+	else // 图
+	{
+		G.arcs[k1][k2].adj = 0;
+	}
+	
+	if(G.kind > 1) //无向
+	{
+		G.arcs[k2][k1].adj = G.arcs[k1][k2].adj;
+	}
+	--G.arcnum;  //弧数加1
+	return OK;
+ 
+}
+
+ //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>图的创建>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  
 Status CreateDG(MGraph &G){
 	//构造有向图
@@ -279,22 +422,17 @@ Status CreateGraph(MGraph &G){
 	default:return ERROR;
 	}
 }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>图的遍历>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 VertexType GetVex(MGraph &G, int v){
 	//初始条件：图G存在，v是G中某个顶点的序号
 	//操作结果：若存在，则返回其值,否则返回空
 	if(v >= G.vexnum || v < 0)
 		return NULL;
 	return G.vexs[v];
-}
- 
-Status PutVex(MGraph &G,int v, VertexType value){
-	//初始条件：图G存在，v是G中某个顶点的序号
-	//操作结果：对v赋值value
-	if(v >= G.vexnum || v < 0)
-		return ERROR;
-	G.vexs[v] = value;
-	return OK;
-}
+} 
+
  
 int FirstAdjVex(MGraph &G, VertexType v){
 	//初始条件：图G存在，v是G中某个顶点
@@ -337,119 +475,7 @@ int NextAdjVex(MGraph &G, VertexType v, VertexType w){
 	return -1;
  
 }
- 
-Status InsertVex(MGraph &G, VertexType v){
-	//初始条件：图G存在，v和图G某个顶点有相同的特征
-	//操作结果：在图G中增添新顶点v（此时不增加关系）
-	int i;
-	G.vexs[G.vexnum] = v;  //赋于顶点值
-	for(i=0; i <=G.vexnum; ++i)
-	{
-		if(G.kind%2==0)//图
-		{
-			G.arcs[G.vexnum][i].adj = 0;
-			G.arcs[i][G.vexnum].adj = 0;
-		}
-		else  //网
-		{
-			G.arcs[G.vexnum][i].adj = INFINITY;
-			G.arcs[i][G.vexnum].adj = INFINITY;
-		}
-	}
-	G.arcs[G.vexnum][i].info = NULL; //初始化该弧的相关信息
-	G.arcs[i][G.vexnum].info = NULL; //初始化该弧的相关信息
-	G.vexnum++;
-	
-	return OK;
-}
- 
-Status DeleteVex(MGraph &G, VertexType v){
-	//初始条件：图G存在，v是G中某个顶点
-	//操作结果：删除G中顶点v及其相关的弧
-	int k = LocateVex(G,v);
-	if(k == -1)
-		return ERROR; //无此结点
-	//1.从顶点向量中删除该顶点
-	int i,j;
-	for(i=k; i < G.vexnum-1; ++i)
-	{
-		G.vexs[i] = G.vexs[i+1];
-	}
- 
-	//2.删除边的关系
-	for(i=k; i < G.vexnum-1; ++i)
-	{
-		for(j=0; j < G.vexnum; ++j)
-		{
-			G.arcs[i][j].adj = G.arcs[i+1][j].adj;
-			//因为没有设弧的相关信息，就不考虑G.arcs[][].info了
-		}
-	}
-	for(j=k; j < G.vexnum-1; ++j)
-	{
-		for(i=0; i < G.vexnum; ++i)
-		{
-			G.arcs[i][j].adj = G.arcs[i][j+1].adj;
-			//因为没有设弧的相关信息，就不考虑G.arcs[][].info了
-		}
-	}
- 
-	--G.arcnum;  //边数减1
-	--G.vexnum;  //顶点数减1
-	return OK;
-}
- 
-Status InsertArc(MGraph &G, VertexType v, VertexType w){
-	//初始条件：图G存在，v和w是G中两个顶点
-	//操作结果：在G中增添弧<v,w>，若是无向的，则还增添对称弧
-	int k1= LocateVex(G,v);
-	int k2= LocateVex(G,w);
-	if(k1==-1 || k2==-1)
-		return ERROR;  //图中不存在v或w
-	if(G.kind%2 == 1)//网
-	{
-		printf("请输入该弧的权值:");
-		scanf("%d",&G.arcs[k1][k2].adj);
-	}
-	else // 图
-	{
-		G.arcs[k1][k2].adj = 1;
-	}
- 
-	if(G.kind > 1) //无向
-	{
-		G.arcs[k2][k1].adj = G.arcs[k1][k2].adj;
-	}
-	++G.arcnum;  //弧数加1
-	return OK;
-	
-}
- 
-Status DeleteArc(MGraph &G, VertexType v, VertexType w){
-	//初始条件：图G存在，v和w是G中两个顶点
-	//操作结果：在G中删除弧<v,w>，若是无向的，则还删除对称弧
-	int k1= LocateVex(G,v);
-	int k2= LocateVex(G,w);
-	if(k1==-1 || k2==-1)
-		return ERROR;  //图中不存在v或w
-	if(G.kind%2 == 1)//网
-	{
-		G.arcs[k1][k2].adj = INFINITY;
-	}
-	else // 图
-	{
-		G.arcs[k1][k2].adj = 0;
-	}
-	
-	if(G.kind > 1) //无向
-	{
-		G.arcs[k2][k1].adj = G.arcs[k1][k2].adj;
-	}
-	--G.arcnum;  //弧数加1
-	return OK;
- 
-}
- 
+
 void DFS(MGraph &G, int v){
 	//从第v个顶点出发递归地深度优先遍历图G
 	visited[v] = TRUE;
@@ -501,29 +527,14 @@ Status VisitVex(MGraph &G,int v){
 	printf("%3c",G.vexs[v]);
 	return OK;
 }
-void DisplayVexValue(const MGraph &G){
-	//显示所有的顶点
-	printf("顶点为：");
-	for(int i=0; i < G.vexnum; ++i)
-		printf("%c\t",G.vexs[i]);
-	printf("\n");
-}
- 
-void DisplayAdjMatrix(const MGraph &G){
-	//输出邻接矩阵
-	printf("\n邻接矩阵为：\n");
-	for(int i=0; i<G.vexnum; ++i){
-		for(int j=0; j<G.vexnum; ++j){
-			if(G.arcs[i][j].adj==INFINITY){
-				printf("∞\t");
-			}
-			else{
-				printf("%d\t",G.arcs[i][j].adj);
-			}
-		}
-		printf("\n");
-	}
-}
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>最小生成树>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+
+typedef struct closedg{
+	VertexType adjvex;
+	VRType lowcost;
+}closedge[MAX_VERTEX_NUM];
 
 int minimum(closedge close,int n){
 	int min=INFINITY,k=0,i;
@@ -667,7 +678,6 @@ int main(int argc,char **argv){
 			default:
 				printf("无此操作\n");	
 		}
- 
 	}
 	return 0;
 }
